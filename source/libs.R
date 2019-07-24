ed4d905ca0c6be2ca6cdfe702d83e3cec6c6935e
Yannis Markonis <imarkonis@gmail.com>
2019-07-24 17:20:42 +0200

    add some libs

--- source/libs.R
+++ source/libs.R
@@ -1,9 +1,10 @@
 # Basic libraries needed - don't modify this for a library just used in one/two scripts
 
 if(!require(pacman))install.packages("pacman")
+if(!require(bbplot))devtools::install_github('bbc/bbplot')
 
-pacman::p_load('dplyr', 'tidyr', 'DescTools',
-               'ggplot2', 'ggalt', 'ggExtra',
+pacman::p_load('tidyverse', 'DescTools', 'RColorBrewer',
+               'ggalt', 'ggExtra', 'data.table', 
                'forcats', 'R.utils', 'png', 
                'grid', 'ggpubr', 'scales',
                'bbplot')
